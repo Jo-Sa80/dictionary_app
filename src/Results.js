7dce824cc07a6c17cc9a39ac3ef248376b799e1c
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-06-12 11:52:12 +0200

    console

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
 export default function Results(props) {
-  console.log(props.data);
   if (props.data) {
     return (
       <div>
@@ -9,15 +8,14 @@ export default function Results(props) {
         <br />
         <h3>Pronunciation: {props.data.phonetic}</h3>
         <br />
-        <p>
-          {props.data.meanings.map(function (meaning, index) {
-            return (
-              <div key={index}>
-                <Meaning meaning={meaning} />
-              </div>
-            );
-          })}
-        </p>
+
+        {props.data.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
