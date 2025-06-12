658d5004c85278687747c4dd81cc9d54f751dbf0
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-06-12 11:00:48 +0200

    second try map

--- src/Results.js
+++ src/Results.js
@@ -5,10 +5,22 @@ export default function Results(props) {
   if (props.data) {
     return (
       <div>
-        <h2>{props.data.word}</h2>
-        {props.data.map(function (meaning, index) {
-          return meaning.meanings[0].definition;
-        })}
+        <h2> {props.data.word}</h2>
+        <br />
+        <h3>Pronunciation: {props.data.phonetic}</h3>
+        <br />
+        <p>
+          {" "}
+          Different definitions for {props.data.word} are:
+          {props.data.meanings.map(function (meaning, index) {
+            return (
+              <ul>
+                {" "}
+                <li key={index}>{meaning.definition}</li>
+              </ul>
+            );
+          })}
+        </p>
       </div>
     );
   } else {
